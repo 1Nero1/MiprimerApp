71b0fc4fc7f78525f393b1034f673a684ff8b3a3
Nero <Ikaros__nymph@hotmail.com>
2022-07-28 00:01:00 -0500

    Creando los primeros eventos "registrar" y "regresar"

--- app/app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+                                        /** ! Sirve apara que todos los componentes sirvan sin tener que hacer
+                                        //      mucho codigo, en este caso para que todos los botones tengan
+                                        //      la misma accion de onclivk **/
+// class MainActivity : AppCompatActivity(), View.onClickListener() {
+class MainActivity : AppCompatActivity(){
+    /**Variables Globales**/
+//    var boton: Button ? = null;
 
-class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**Evento para Registrarse**/
+        val btn: Button = findViewById(R.id.Registrar)
+        btn.setOnClickListener {
+            val ev: Intent = Intent(this,formReg:: class.java)
+            startActivity(ev)
+        }
+
     }
-    
+    /**Metodo para los botones**/
+
+    /** llamando para registro
+    fun btnRegistrar(view: View) {
+        // Do something in response to button click
+    }
+     */
 }
--- app/app/src/main/java/com/example/myapp/formReg.kt
+++ app/app/src/main/java/com/example/myapp/formReg.kt
@@ -1,11 +1,21 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class formReg : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_reg)
+
+        /**Evento para Regresar**/
+        val btn_reg: Button = findViewById(R.id.btn_regresar)
+        btn_reg.setOnClickListener {
+            val ev: Intent = Intent(this,MainActivity:: class.java)
+            startActivity(ev)
+        }
     }
+
 }
