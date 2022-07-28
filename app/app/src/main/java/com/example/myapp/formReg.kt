bef917c6729f2dfa4eaf64a7156ac7b635ff14eb
Nero <Ikaros__nymph@hotmail.com>
2022-07-27 21:58:03 -0500

    Pintando segunda pantalla (Registro)

--- app/app/src/main/java/com/example/myapp/formReg.kt
+++ app/app/src/main/java/com/example/myapp/formReg.kt
@@ -3,10 +3,9 @@ package com.example.myapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class formReg : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_form_reg)
     }
-    
 }
